f7210c2dba4c7f75958430d4439e89c8082b5b94
Romeo Stoll <47355990+rstol@users.noreply.github.com>
2025-03-05 14:01:46 +0100

    delete translate collection

--- src/api/translate/routes/translate.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * translate router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::translate.translate');
--- src/api/translate/services/translate.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * translate service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::translate.translate');
